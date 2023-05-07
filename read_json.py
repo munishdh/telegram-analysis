52436ba8a1262229b123a63ca3df5be7a8140c50
Munish Dhawan <munishdh@gmail.com>
2023-05-07 17:40:37 +0800

    channel_messages.json read

---
+++ read_json.py
@@ -0,0 +1,12 @@
+import json
+import pprint
+
+json_filename = 'channel_messages.json'
+
+with open(json_filename, encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+for item in data:
+    if item.get('message'):
+        if 'Alert for ST COMBO HTF' in item['message']:
+            print(item['message'])
